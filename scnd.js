2d386d9038d17e163a910931aa7a1aa56b672073
24LP5169 24LP5169 <24LP5169@G304-CC0226.local>
2024-11-19 12:41:37 +0800

    angideer hiisn

---
+++ scnd.js
@@ -0,0 +1,54 @@
+// 1dh dasgal alimni nit une bananii niiit une iig oloh
+let apple = 15;
+let banana = 10;
+let applepr = 1.5;
+let bananapr = 0.8;
+console.log("niit alim:", apple * applepr, "niit banana:", banana * bananapr);
+
+// 2dh dasgal eyreg surg sondgoi tegsh oloh
+let x = -103;
+if (x % 2 == 0) {
+  if (x > 0) {
+    console.log("ene eyreg bolon tegsh too");
+  } else {
+    console.log("ene eyreg bolon surug too");
+  }
+} else {
+  if (x > 0) {
+    console.log("ene sondgoi bolon eyreg too");
+  } else {
+    console.log("ene surug bolon sondgoi too");
+  }
+}
+
+// 3dh dasgal login system
+let name = "tuguldur";
+let pass = 123;
+if (name == "tuguldur") {
+  if (pass == 123) {
+    console.log("ta newterlee");
+  } else {
+    console.log("tanii pass buruu bn");
+  }
+} else {
+  if (pass == 123) {
+    console.log("tanii ner buruu bn");
+  } else {
+    console.log("tanii ner pass 2la buruu bn");
+  }
+}
+
+//  4dh dasgal child teeneg adult oloh
+let person = 11;
+if (person > 13) {
+  if (person < 19) {
+    console.log("uswur nasnii huuhed");
+  } else {
+    console.log("ta nasand hursen");
+  }
+} else {
+  console.log("ta huuhed ");
+}
+//  5dh dasgal the 4 operations and print the result.
+let num1 = 10;
+let num2 = 21;
