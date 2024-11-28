5835b154759f14fc6471562b5ca1a21443e1bebb
NixBaBy <115783986+NixBaBy@users.noreply.github.com>
2024-11-28 21:30:25 +0800

    gert hiiv

--- array-methods.js
+++ array-methods.js
@@ -1,49 +1,35 @@
 // tegsh hamgiin baga toog ol
 
-let ages = [10, 2, 865, 123, 8, 5, 32, 876, 234, 9];
-const findMinNumbers = (input) => {
-  let minNum = input[0];
-  for (let i = 0; i < input.length; i++) {
-    if (minNum > input[i + 1]) {
-      minNum = input[i + 1];
-    }
-  }
-  return minNum;
-};
-const findEven = (input) => {
-  let evenNum = [];
-  for (let i = 1; i < input.length; i++) {
-    if (input[i] % 2 != 0) {
-      evenNum.push(input[i]);
-    }
-  }
-  return evenNum;
-};
-let answer = findEven(ages);
-let answer2 = findMinNumbers(answer);
-console.log(answer2);
+// let ages = [10, 2, 865, 123, 8, 5, 32, 876, 234, 9];
+// const findMinNumbers = (input) => {
+//   let minNum = input[0];
+//   for (let i = 0; i < input.length; i++) {
+//     if (minNum > input[i + 1]) {
+//       minNum = input[i + 1];
+//     }
+//   }
+//   return minNum;
+// };
+// const findEven = (input) => {
+//   let evenNum = [];
+//   for (let i = 1; i < input.length; i++) {
+//     if (input[i] % 2 != 0) {
+//       evenNum.push(input[i]);
+//     }
+//   }
+//   return evenNum;
+// };
+// let answer = findEven(ages);
+// let answer2 = findMinNumbers(answer);
+// console.log(answer2);
 
-// sondgoi hamgiin ih toog ol
-let scores = [10, 2, 865, 123, 8, 5, 32, 876, 234, 9];
-const findOddNumbers = (input) => {
-  let oddNumber = [];
-  for (let i = 1; i < input.length; i++) {
-    if (input[i] % 2 != 0) {
-      oddNumber.push(input[i]);
-    }
-  }
-  return oddNumber;
-};
-const findMaxNumbers = (input) => {
-    MaxNumber = 
-}
+//  orlogo zarlaga ylgadag  ***************************
 
-//  orlogo zarlaga ylgadag
-let guilge = [
-  12, 456, 758, 1237, 8652, 4536, -1245366, -1325, -1246, -138, -124,
-];
+let guilge = [12, 456, 758, 1237, 8652, 4536, -1366, -1325, -1246, -138, -124];
+// orlogo oloh function
 const findOrlogo = (input) => {
   let orlogo = [];
+
   for (let i = 1; i < input.length; i++) {
     if (input[i] > 0) {
       orlogo.push(input[i]);
@@ -52,8 +38,19 @@ const findOrlogo = (input) => {
   return orlogo;
 };
 let answer = findOrlogo(guilge);
-console.log("orlogonuud", answer);
 
+console.log("orlogonuud:", answer);
+// niit orlogo oldog function
+const totalOrlogo = (num) => {
+  let totalOrlogo = 0;
+  for (let i = 1; i < num.length; i++) {
+    totalOrlogo = totalOrlogo + num[i];
+  }
+  return totalOrlogo;
+};
+let answer2 = totalOrlogo(answer);
+console.log("niit orlogo:", answer2);
+// zarlaga oldog function
 const findZarlaga = (input) => {
   let zarlaga = [];
   for (let i = 1; i < input.length; i++) {
@@ -65,3 +62,14 @@ const findZarlaga = (input) => {
 };
 let answer1 = findZarlaga(guilge);
 console.log("zarlagnuud", answer1);
+
+// niit zarlaga oldog function
+const findTotalZarlaga = (input) => {
+  let totalZarlaga = 0;
+  for (let i = 0; i < input.length; i++) {
+    totalZarlaga = totalZarlaga + input[i];
+  }
+  return totalZarlaga;
+};
+let answer3 = findTotalZarlaga(answer1);
+console.log("niit zarlaga", answer3);
--- array.js
+++ array.js
@@ -14,29 +14,46 @@ function findAverageAge(ages) {
   console.log("dundaj ni:", sumAges / ages.length);
 }
 findAverageAge(ages);
-//  array dotrh hamgiin baga utgiig ol
-// array dotorh hamgiin baga gehdee sondgoi utgiig ol
+
 // array dotorh hamgiiin ih tegsh toog ol
 
 //  array dotrh hamgiin baga utgiig ol
-// let numbers = [40, 20, 30, 46, 235, 87, 233, 98, 127, 10];
-// const findLittle = (arr) => {
-//   let littleNum = arr[0];
-//   for (let i = 0; i < arr.length; i++) {
-//     if (littleNum > arr[i + 1]) {
-//       littleNum = arr[i + 1];
+
+// let scores = [20, 20, 30, 40, 50, 10, 5, 3, 15];
+// const findMinNumbers = (num) => {
+//   let minNum = num[0];
+//   for (let i = 0; i < num.length; i++) {
+//     if (minNum > num[i + 1]) {
+//       minNum = num[i + 1];
 //     }
 //   }
-//   console.log(littleNum);
+//   return minNum;
 // };
-// findLittle(numbers);
+// let answer = findMinNumbers(scores);
+// console.log(answer);
 
 // array dotorh hamgiin baga gehdee sondgoi utgiig ol
-let numbers = [44, 5, 25, 46, 235, 87, 233, 98, 127, 11];
-const findMin = (arr) => {
-  let minNum = arr[0];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 == 0 || minNum > arr[i])
+
+let scores = [2, 20, 30, 40, 50, 10, 5, 4, 15];
+const findMinOdd = (num) => {
+  let oddNum = [];
+  for (let i = 0; i < num.length; i++) {
+    if (num[i] % 2 != 0) {
+      oddNum.push(num[i]);
+    }
   }
+  return oddNum;
 };
-let answer1 = findMin(numbers);
+let answer = findMinOdd(scores);
+
+const findMinNumbers = (num) => {
+  let minNum = num[0];
+  for (let i = 0; i < num.length; i++) {
+    if (minNum > num[i + 1]) {
+      minNum = num[i + 1];
+    }
+  }
+  return minNum;
+};
+let answer1 = findMinNumbers(answer);
+console.log(answer1);
