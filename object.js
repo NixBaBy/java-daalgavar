0d80944126742ff7dc79ce0f48d1bf59562cde82
24LP5169 24LP5169 <24LP5169@G304-CC0229.local>
2024-11-29 12:13:12 +0800

    hichelder hiiw

--- object.js
+++ object.js
@@ -1,29 +1,62 @@
+// let students = [
+//   { name: "dorjoo", age: 15, gender: "male" },
+//   { name: "dulma", age: 30, gender: "female" },
+//   { name: "dondog", age: 40, gender: "male" },
+//   { name: "bata", age: 10, gender: "male" },
+//   { name: "tsetsge", age: 25, gender: "female" },
+// ];
+// const findMale = (input) => {
+//   let genderMale = [];
+//   for (let i = 0; i < input.length; i++) {
+//     if (input[i].gender == "male") {
+//       genderMale.push(input[i]);
+//     }
+//   }
+//   return genderMale;
+// };
+// let answer = findMale(students);
+// console.log("Males", answer);
+// const findFemale = (input) => {
+//   let genderFemale = [];
+//   for (let i = 0; i < input.length; i++) {
+//     if (input[i].gender == "female") {
+//       genderFemale.push(input[i]);
+//     }
+//   }
+//   return genderFemale;
+// };
+// let answer1 = findFemale(students);
+// console.log("Females", answer1);
+
+//  suragchdiin dungiin dundaj ol
 let students = [
-  { name: "dorjoo", age: 15, gender: "male" },
-  { name: "dulma", age: 30, gender: "female" },
-  { name: "dondog", age: 40, gender: "male" },
-  { name: "bata", age: 10, gender: "male" },
-  { name: "tsetsge", age: 25, gender: "female" },
+  { name: "boldo", age: 15, grades: [100, 100, 100, 100, 100], gender: "male" },
+  { name: "tugu", age: 16, grades: [90, 90, 90, 90, 90], gender: "male" },
+  {
+    name: "sapara",
+    age: 17,
+    grades: [100, 100, 100, 100, 100],
+    gender: "male",
+  },
 ];
-const findMale = (input) => {
-  let genderMale = [];
-  for (let i = 0; i < input.length; i++) {
-    if (input[i].gender == "male") {
-      genderMale.push(input[i]);
-    }
+const findOneStudentAverageGrade = (oneStudent) => {
+  let grades = oneStudent.grades;
+  let num = 0;
+  for (let i = 0; i < grades.length; i++) {
+    num = num + grades[i];
   }
-  return genderMale;
+  let average = num / grades.length;
+  return average;
 };
-let answer = findMale(students);
-console.log("Males", answer);
-const findFemale = (input) => {
-  let genderFemale = [];
-  for (let i = 0; i < input.length; i++) {
-    if (input[i].gender == "female") {
-      genderFemale.push(input[i]);
-    }
+
+const findAllStudentAverageGrade = (allStudent) => {
+  let num = 0;
+  for (let i = 0; i < students.length; i++) {
+    let average = findOneStudentAverageGrade(allStudent[i]);
+    num = num + average;
   }
-  return genderFemale;
+  let allAverage = num / allStudent.length;
+  return allAverage;
 };
-let answer1 = findFemale(students);
-console.log("Females", answer1);
+let answer = findAllStudentAverageGrade(students);
+console.log(answer);
